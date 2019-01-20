50c129ab7d586573740d3697eb6f691075809226
syspwn.github.io <33608588+0strich@users.noreply.github.com>
2019-01-20 16:31:25 +0900

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -3,5 +3,5 @@
 @ python-2.7.13.msi
 @ pause
 
-@ move _vimrc C:\Program Files (x86)\Vim
-@ move jellybeans.vim C:\Program Files (x86)\Vim\vim81\colors
+@ move _vimrc C:\"Program Files (x86)"\Vim
+@ move jellybeans.vim C:\"Program Files (x86)"\Vim\vim81\colors
