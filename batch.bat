3a4ebde9ff35740c9566ddd55617ad705aad1599
0strich <gee8195@naver.com>
2019-01-22 20:00:56 +0900

    updated at 1/22 20:00

--- batch.bat
+++ batch.bat
@@ -1,7 +1,10 @@
 @ gvim81.exe
 
 @ python-2.7.13.msi
-@ pause
 
 @ move _vimrc C:\"Program Files (x86)"\Vim
 @ move jellybeans.vim C:\"Program Files (x86)"\Vim\vim81\colors
+
+setx path "%PATH%;C:\Python27;C:\Python27\include"
+
+@ pause
